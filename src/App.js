4a6d9785442e814daf4dda60a4c3a64ad27bf5a8
BaselQq <73541125+BaselQq@users.noreply.github.com>
2022-07-25 21:01:46 +0200

    added firebase api to fetch data

--- src/App.js
+++ src/App.js
@@ -4,12 +4,48 @@ import { useState } from "react";
 
 function App() {
   const [tasks, setTasks] = useState([]);
-  const onAdd = (task) => setTasks([...tasks, task]);
+  const onAdd = async (task) => {
+    setTasks([...tasks, task]);
+    const response = await fetch(
+      "FirebaseAPILink/todolist.json",
+      {
+        method: "POST",
+        body: JSON.stringify(task),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+    console.log(data);
+  };
   const onDelete = (id) => setTasks(tasks.filter((task) => task.id !== id));
-  const onEdit = (id) => setTasks(tasks.map((task) => task.id === id ? { ...task, edited: true } : { ...task }));
-  const onDone = (id) => setTasks(tasks.map((task) => task.id === id ? { ...task, done: !task.done } : { ...task }));
-  const onEditDone = (newTask) => setTasks(tasks.map((task) => task.id === newTask.id ? { ...task, name: newTask.name, edited: false } : { ...task }));
-  const onEditCancel = (id) => setTasks(tasks.map((task) => task.id === id ? { ...task, edited: false } : { ...task }));
+  const onEdit = (id) =>
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, edited: true } : { ...task }
+      )
+    );
+  const onDone = (id) =>
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, done: !task.done } : { ...task }
+      )
+    );
+  const onEditDone = (newTask) =>
+    setTasks(
+      tasks.map((task) =>
+        task.id === newTask.id
+          ? { ...task, name: newTask.name, edited: false }
+          : { ...task }
+      )
+    );
+  const onEditCancel = (id) =>
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, edited: false } : { ...task }
+      )
+    );
 
   return (
     <div className="App">
